b778e4a087ca005a5c22e339f326ed66d1e8eabf
403F2E <shadowouss1@gmail.com>
2026-02-24 09:21:37 +0000

    feat: remove undetected plugin

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -46,20 +46,6 @@ local on_attach = function(client)
   end
 end
 
--- Enable ALL LSP capabilities
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
-capabilities.textDocument = {
-  completion = {
-    completionItem = {
-      snippetSupport = true,
-      resolveSupport = {
-        properties = { "documentation", "detail", "additionalTextEdits" },
-      },
-    },
-  },
-}
-
 -- EXAMPLE
 local servers = { "html", "cssls" }
 
